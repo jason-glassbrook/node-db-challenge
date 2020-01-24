e1d9acb08d27ca6cdde07c6d5374d3f5a5536d4c
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-24 13:05:35 -0500

    add routes for /resources and /tasks

---
+++ routes/api/tasks/router.js
@@ -0,0 +1,41 @@
+/***********************************************************
+  ~/tasks - router
+***********************************************************/
+
+const express = require ('express')
+
+/***************************************
+  setup router
+***************************************/
+
+const router = express.Router ()
+
+/// requests ///
+
+router.route ('/')
+.get ((ri, ro) => {
+  ro
+  .status (501)
+  .json ({
+    'error' : {
+      'message' : 'working on it...',
+      'method' : ri.method,
+      'route' : ri.originalUrl,
+    }
+  })
+})
+.post ((ri, ro) => {
+  ro
+  .status (501)
+  .json ({
+    'error' : {
+      'message' : 'working on it...',
+      'method' : ri.method,
+      'route' : ri.originalUrl,
+    }
+  })
+})
+
+/**************************************/
+
+module.exports = router
