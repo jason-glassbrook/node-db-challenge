56d45dab434df641e7bd8e14157d33e61a62ae7e
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-24 21:17:25 -0500

    strip project_id from value

--- models/api/resources/push.js
+++ models/api/resources/push.js
@@ -3,12 +3,19 @@ const get = require ('./get')
 
 module.exports =
   async (resource_value) => {
+    const { project_id } = resource_value
+
+    resource_value = {
+      'name' : resource_value.name,
+      'description' : resource_value.description,
+    }
+
     const [ resource_id ] = await (
       data ('resources')
       .insert (resource_value)
     )
 
-    const resource_record = await get (resource_id)
+    let resource_record = await get (resource_id)
 
     return resource_record
   }
--- models/api/tasks/push.js
+++ models/api/tasks/push.js
@@ -3,12 +3,20 @@ const get = require ('./get')
 
 module.exports =
   async (task_value) => {
+    const { project_id } = task_value
+
+    task_value = {
+      'description' : task_value.description,
+      'notes' : task_value.notes,
+      'completed' : task_value.completed,
+    }
+
     const [ task_id ] = await (
       data ('tasks')
       .insert (task_value)
     )
 
-    const task_record = await get (task_id)
+    let task_record = await get (task_id)
 
     return task_record
   }
