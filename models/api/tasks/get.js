3c3f6e61fc30d7f241bdf85663aa8e741742699e
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-24 21:32:34 -0500

    return project's name and description with tasks

--- models/api/tasks/get.js
+++ models/api/tasks/get.js
@@ -4,8 +4,19 @@ const boolify = require ('../boolify')
 module.exports =
   async (task_id) => {
     let [ task_record ] = await (
-      data ('tasks')
-      .where ({ id : task_id })
+      data
+      .select ([
+        't.id',
+        't.description',
+        't.notes',
+        't.completed',
+        'p.name as project_name',
+        'p.description as project_description',
+      ])
+      .from ('projects_tasks as p_t')
+      .join ('projects as p', 'p.id', '=', 'p_t.project_id')
+      .join ('tasks as t', 't.id', '=', 'p_t.task_id')
+      .where ({ 't.id' : task_id })
     )
 
     task_record = boolify (task_record, 'completed')
--- models/api/tasks/getAll.js
+++ models/api/tasks/getAll.js
@@ -4,7 +4,18 @@ const boolify = require ('../boolify')
 module.exports =
   async () => {
     let task_records = await (
-      data ('tasks')
+      data
+      .select ([
+        't.id',
+        't.description',
+        't.notes',
+        't.completed',
+        'p.name as project_name',
+        'p.description as project_description',
+      ])
+      .from ('projects_tasks as p_t')
+      .join ('projects as p', 'p.id', '=', 'p_t.project_id')
+      .join ('tasks as t', 't.id', '=', 'p_t.task_id')
     )
 
     task_records = task_records.map (
