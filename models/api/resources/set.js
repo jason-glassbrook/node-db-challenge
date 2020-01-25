919adc4e3661d29152933dd350ac39e41ace05e2
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-24 21:19:35 -0500

    strip project_id from value

--- models/api/resources/set.js
+++ models/api/resources/set.js
@@ -3,6 +3,14 @@ const get = require ('./get')
 
 module.exports =
   async (resource_id, resource_value) => {
+    const { project_id } = resource_value
+
+    resource_value = {
+      'description' : resource_value.description,
+      'notes' : resource_value.notes,
+      'completed' : resource_value.completed,
+    }
+
     await (
       data ('resources')
       .where ({ id : resource_id })
--- models/api/tasks/set.js
+++ models/api/tasks/set.js
@@ -3,6 +3,14 @@ const get = require ('./get')
 
 module.exports =
   async (task_id, task_value) => {
+    const { project_id } = task_value
+
+    task_value = {
+      'description' : task_value.description,
+      'notes' : task_value.notes,
+      'completed' : task_value.completed,
+    }
+
     await (
       data ('tasks')
       .where ({ id : task_id })
